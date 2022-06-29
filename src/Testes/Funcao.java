f578c3dc3a4ebcec2316a6743db94adda8960df5
Icaro <100494049+Icaroiz@users.noreply.github.com>
2022-06-29 15:41:30 -0300

    TESTE

---
+++ src/Testes/Funcao.java
@@ -0,0 +1,25 @@
+package Testes;
+
+public class Funcao {
+		
+	public static void main(String[]args) {
+		
+		System.out.println("Vai comecar a contagem");
+		System.out.println(Operacoes.contador(1,5));
+	}	
+}
+
+
+/*static int soma(int a, int b){
+	int s = a + b;
+	return s;
+	System.out.println("Comecou o programa ");
+	int sm = soma(5,2);
+	System.out.println("A soma vale: " + sm);}*/
+
+
+
+
+
+
+
